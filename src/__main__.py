c932acf3dfb450b3d1ce25a6da501304f7a07cb8
Jordan Mohler <jordan.mohler@allata.com>
2020-06-10 23:00:50 -0600

    Clean up

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,14 @@
-from .scripts.basic_comparison import *
-from .scripts.excel_parser import *
-# from .scripts.recommender import *
+# from .scripts.basic_comparison import *
+from .scripts.excel_parser import parse_to_dictionary, visualize
+from .scripts.recommender import recommender
 
 workbook_path = 'src/data/Sample_Movie_Ratings.xlsx'
 
 user_ratings = parse_to_dictionary(workbook_path)
 
-visualize(user_ratings)
+# visualize(user_ratings)
+# print(user_ratings);
+
+r = recommender(user_ratings)
+
+print(r.recommend('Brian'))
