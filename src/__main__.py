3a6ef1e8160f9bd13468456a3f94ec4c01b6e05b
Jordan Mohler <jordan.mohler@allata.com>
2020-06-11 22:29:50 -0600

    Clear test code

--- src/__main__.py
+++ src/__main__.py
@@ -6,14 +6,3 @@ from .scripts.visualization import visualizeCorrelation
 workbook_path = 'src/data/Sample_Movie_Ratings.xlsx'
 
 user_ratings = parse_to_dictionary(workbook_path)
-
-k = 3
-n = 5
-metric = 'pearson'
-
-r = recommender(user_ratings, k=k, n=n, metric=metric)
-
-for user in ['Zak', 'Brian', 'Patrick C']:
-    print('\n{0}\'s {1} nearest neighbors: '.format(user, k), r.computeNearestNeighbor(user)[:k], '. They recommend ', r.recommend(user))
-
-# visualizeCorrelation(user_ratings, "Zak", "Vanessa")
