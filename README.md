d2be7afc814e0627dd3e53427d9d8db0a78b6bd2
Jordan Mohler <jordan.mohler@allata.com>
2020-06-11 22:42:19 -0600

    Remove unused command from readme

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-exec(open('recommendation.py').read())
-
 pip freeze > requirements.txt
 pip install -r requirements.txt
--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
-# from .scripts.basic_comparison import *
+from .scripts.basic_comparison import *
 from .scripts.excel_parser import parse_to_dictionary
-from .scripts.recommender import recommender
+# from .scripts.recommender import recommender
 from .scripts.visualization import visualizeCorrelation
 
 workbook_path = 'src/data/Sample_Movie_Ratings.xlsx'
